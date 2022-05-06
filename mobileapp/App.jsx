6bbb640392e8f2cda6150abd160e0521b5ae729c
Kalin Chervenkov <KSChervenkov19@codingburgas.bg>
2022-05-06 10:51:59 +0300

    Add navigation

--- mobileapp/App.jsx
+++ mobileapp/App.jsx
@@ -1,38 +1,75 @@
-import React from "react";
-import { StatusBar } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Alert,
+  StatusBar,
+  Text,
+  Image,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Map } from "./components/Map";
-import { EventSearch } from "./components/EventSearch";
-import { NavigatorContext } from "./components/NavigatorContext";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MapView from "react-native-maps";
+import * as Location from "expo-location";
+import Svg, { Path, Circle } from "react-native-svg";
 
 <StatusBar translucent backgroundColor="transparent" />;
-const Stack = createNativeStackNavigator();
-
-function MapComponent({ navigation }) {
-  return (
-    <NavigatorContext.Provider value={navigation}>
-      <Map />
-    </NavigatorContext.Provider>
-  );
-}
-
-function EventComponent({ navigation }) {
-  return <EventSearch />;
-}
+const Stack = createBottomTabNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveTintColor: "#e91e63",
+          tabBarStyle: { height: 112 },
+        }}
+      >
         <Stack.Screen
-          name="MapComponent"
-          component={MapComponent}
+          name="Map"
+          component={Map}
+          options={{
+            tabBarLabel: " ",
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                style={{
+                  width: 35,
+                  height: 44,
+                }}
+                source={require("./assets/images/mapIconSelected.png")}
+              ></Image>
+            ),
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="  "
+          component={Events}
+          options={{
+            tabBarLabel: " ",
+            tabBarIcon: ({ color, size }) => (
+              <Image
+                style={{
+                  width: 45,
+                  height: 45,
+                }}
+                source={require("./assets/images/searchIconSelected.png")}
+              ></Image>
+            ),
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="   "
+          component={Notifications}
           options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="EventComponent"
-          component={EventComponent}
+          name="    "
+          component={User}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
@@ -40,4 +77,100 @@ function App() {
   );
 }
 
-export { App };
+function Map() {
+  const [location, setLocation] = useState(null);
+  const [userLocationLat, setUserLocationLat] = useState(0);
+  const [userLocationLong, setUserLocationLong] = useState(0);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert(
+          "No location access",
+          "Allow Yottabyte to access your location or turn on the location of the device",
+          [
+            {
+              text: "Cancel",
+              style: "cancel",
+            },
+            { text: "Open settings", onPress: () => Linking.openSettings() },
+          ]
+        );
+      }
+
+      const location = await Location.getLastKnownPositionAsync({
+        accuracy: 6,
+        distanceInterval: 1,
+        timeInterval: 1000,
+      });
+      setLocation(location);
+      if (location == null && userLocationLat != -80) {
+        setUserLocationLat(-80);
+        setUserLocationLong(-176);
+      } else if (userLocationLat != location.coords.latitude) {
+        setUserLocationLat(location.coords.latitude);
+        setUserLocationLong(location.coords.longitude);
+      }
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        mapType="satellite"
+        paddingAdjustmentBehavior="automatic"
+        toolbarEnabled={false}
+        userLocationPriority="high"
+        userLocationFastestInterval={1000}
+        userLocationUpdateInterval={1000}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        initialRegion={{
+          latitude: 42.4991874040583,
+          longitude: 27.46135711669922,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+}
+
+function Events() {
+  return (
+    <>
+      <Text>testink...</Text>
+    </>
+  );
+}
+
+function Notifications() {
+  return (
+    <>
+      <Text>testink...</Text>
+    </>
+  );
+}
+
+function User() {
+  return (
+    <>
+      <Text>testink...</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("screen").height,
+  },
+});
+
+export default App;
--- mobileapp/components/NavigatorContext.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const NavigatorContext = React.createContext(null);
